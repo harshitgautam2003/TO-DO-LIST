bb19b7afe1b08ce6b0210f2e68f05ebac5a4c928
Harshit GAutam <harshitgtm02@gmail.com>
2022-09-19 19:37:23 +0530

    rep

--- ind.js
+++ ind.js
@@ -4,6 +4,7 @@ const taskS=document.querySelector(".todo-list");
 const submitButton=document.querySelector("#submit")
 const clearButton=document.querySelector("#clear")
 
+document.addEventListener("DOMContentLoaded",getTasks)
 submitButton.addEventListener("click",addTask)
 clearButton.addEventListener("click",cleaR)
 taskS.addEventListener('click',delCheck);
@@ -12,9 +13,11 @@ function addTask(){
     todoDiv.classList.add("todo");
     
     const newTask=document.createElement('li');
-    newTask.textContent=inputTask.value+" /--"+inputDate.value;
+    newTask.textContent=inputTask.value+" || "+inputDate.value;
     newTask.classList.add('newItem');
     todoDiv.appendChild(newTask);
+
+    saveLocal(inputTask.value);
 
     const checkButton=document.createElement('button');
     checkButton.innerHTML='<i class="fas fa-check"></i>'
@@ -29,6 +32,7 @@ function addTask(){
     taskS.appendChild(todoDiv);
     inputTask.value="";
     inputDate.value="";
+
 }
 function delCheck(e){
     const item=e.target;
@@ -44,4 +48,48 @@ function delCheck(e){
 }
 function cleaR(){
     taskS.textContent=null;
+}
+
+function saveLocal(todo){
+   let todoList;
+   if(localStorage.getItem("todoList")===null){
+        todoList=[];
+   }
+   else{
+    todoList=JSON.parse(localStorage.getItem("todoList"));
+   }
+   todoList.push(todo);
+   localStorage.setItem("todoList",JSON.stringify(todoList));
+}
+function getTasks(){
+    let todoList;
+   if(localStorage.getItem("todoList")===null){
+        todoList=[];
+   }
+   else{
+    todoList=JSON.parse(localStorage.getItem("todoList"));
+   }
+   todoList.forEach(function (todo) {
+    const todoDiv=document.createElement("div");
+    todoDiv.classList.add("todo");
+    
+    const newTask=document.createElement('li');
+    newTask.textContent=todo;
+    newTask.classList.add('newItem');
+    todoDiv.appendChild(newTask);
+
+    
+
+    const checkButton=document.createElement('button');
+    checkButton.innerHTML='<i class="fas fa-check"></i>'
+    checkButton.classList.add("check-btn");
+    todoDiv.appendChild(checkButton);
+
+    const delButton=document.createElement('button');
+    delButton.innerHTML='<i class="fas fa-trash"></i>'
+    delButton.classList.add("del-btn");
+    todoDiv.appendChild(delButton);
+
+    taskS.appendChild(todoDiv);
+   });
 }
